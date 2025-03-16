2b314cd46e652320fcf4a80d77c50d597fcc3964
Nkemka Akah <nkemkaomeiza@gmail.com>
2025-03-16 00:09:26 +0000

    initial commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text className="text-white">Index</Text>
+    </SafeAreaView>
+  );
+}
+
+
